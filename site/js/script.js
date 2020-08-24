9c20678798e4b82e267d3e00830b07a02505e702
Flor Espino <fespino@perceptyx.com>
2020-08-24 02:12:08 -0300

    mobile ready

---
+++ site/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.js-covid-slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: false,
+		nextArrow:"<img class='a-right control-c next slick-next' src='img/icons/right_dark.png'>",
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='img/icons/left_dark.png'>"
+	});
+});
+
+
+
+
